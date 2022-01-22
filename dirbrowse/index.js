6679c2b32b3a55807b98bb82936987b5e0d7f5b2
Eric Rangell <erangell@gmail.com>
2022-01-22 15:15:31 -0500

    Fixed bug with spaces in filenames

--- dirbrowse/index.js
+++ dirbrowse/index.js
@@ -21,59 +21,8 @@ async function main(drivekeyStr) {
     client.replicate(drive.metadata)
 
     http.createServer( async (req, res) => {
-        const path = req.url
-
-        const st = await drive.promises.stat(path)
-        //console.log(st)
-        //console.log('File?',st.isFile())
-        //console.log('Dir?',st.isDirectory())
-
-        if (st.isDirectory())
-        {
-            if (!path.endsWith('/'))
-            {
-                return res.writeHead(303, {Location: path + '/' }).end()
-            }
-            const subfiles = await drive.promises.readdir(path)
-
-            //console.log('Directory of: ',path)
-            //console.log(subfiles.join('\n'))
-            
-            const html = `<html><body>
-                ${path !== '/' ? `
-                    <div><a href="..">..</a></div>
-                ` : ''}
-                ${subfiles.map( file => {
-                  return `
-                  <div><a href="${file}">${file}</a></div>
-                  `}).join('')  
-                }
-            </body></html>`
-
-            res.writeHead(200,{'Content-Type' : 'text/html'}).end(html)
-        }
-        else if (st.isFile())
-        {
-            //console.log('Is a file')
-            //res.writeHead(200).end('ToDo')
-
-            res.writeHead(200)
-            
-            //The following reads the whole file before
-            //sending it to the response stream:
-            //const content = await drive.promises.readFile(path)
-            //res.end(content)
-
-            //Every time a chunk is read from the drive
-            //it gets piped to the response stream:
-            
-            console.log('Path:',path)
-            drive.createReadStream(path).pipe(res)
-        }
-
-        //const files = await drive.promises.readdir('/')
-        //res.writeHead(200).end('<b>dZeZ Rocks!</b><pre>'+files.join('\n')+'</pre>')
-
+        serveDrive(drive, req, res)
+        
     }).listen(8080)
     
     console.log('Open web browser to http://localhost:8080')
@@ -87,5 +36,57 @@ main('27091469efe686137e6a54adc4378e9a866721f93821ad9c490a7e349c0dbca3')
 
 async function serveDrive (drive, req, res)
 {
+    const path = req.url
+    console.log('Path:',path)
+    unencpath = decodeURI(path)
+    console.log("Decoded:",unencpath)
     
+    const st = await drive.promises.stat(unencpath)
+    //console.log(st)
+    //console.log('File?',st.isFile())
+    //console.log('Dir?',st.isDirectory())
+
+    if (st.isDirectory())
+    {
+        if (!path.endsWith('/'))
+        {
+            return res.writeHead(303, {Location: path + '/' }).end()
+        }
+        const subfiles = await drive.promises.readdir(path)
+
+        //console.log('Directory of: ',path)
+        //console.log(subfiles.join('\n'))
+        
+        const html = `<html><body>
+            ${path !== '/' ? `
+                <div><a href="..">..</a></div>
+            ` : ''}
+            ${subfiles.map( file => {
+              return `
+              <div><a href="${file}">${file}</a></div>
+              `}).join('')  
+            }
+        </body></html>`
+
+        res.writeHead(200,{'Content-Type' : 'text/html'}).end(html)
+    }
+    else if (st.isFile())
+    {      
+        //console.log('Is a file')
+        //res.writeHead(200).end('ToDo')
+
+        res.writeHead(200)
+        
+        //The following reads the whole file before
+        //sending it to the response stream:
+        //const content = await drive.promises.readFile(path)
+        //res.end(content)
+
+        //Every time a chunk is read from the drive
+        //it gets piped to the response stream:
+        drive.createReadStream(unencpath).pipe(res)
+    }
+
+    //const files = await drive.promises.readdir('/')
+    //res.writeHead(200).end('<b>dZeZ Rocks!</b><pre>'+files.join('\n')+'</pre>')
 }
